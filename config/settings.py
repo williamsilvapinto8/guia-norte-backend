50e1250c84be193678e338c124ea82dfbf871a8e
William Silva Pinto <william.silvapinto@gmail.com>
2026-03-10 07:51:10 -0300

    Refactor database settings configuration
    Removed conditional database configuration for SQLite and PostgreSQL.

--- config/settings.py
+++ config/settings.py
@@ -76,27 +76,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-
-
-if os.environ.get("USE_SQLITE") == "0":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB", "guia_norte"),
-            "USER": os.environ.get("POSTGRES_USER", "postgres"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
-            "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
-            "PORT": os.environ.get("POSTGRES_PORT", "5432"),
-        }
-    }
-##############################
 # Banco de dados
 USE_SQLITE = os.environ.get('USE_SQLITE', '0') == '1'
 
