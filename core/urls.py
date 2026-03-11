a76a190c6a0524f0bc74a8dd3c6c45f09d0e7f97
William Silva Pinto <william.silvapinto@gmail.com>
2026-03-11 13:43:06 -0300

    Fix formatting in urls.py for import statements

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     UserProfileViewSet, BusinessViewSet, BusinessStageHistoryViewSet,
     StageStatusViewSet, FormResponseViewSet, DiagnosisViewSet,
-    ExperimentViewSet, N8NHealthCheckView
+    ExperimentViewSet, N8NHealthCheckView,
 )
 
 router = DefaultRouter()
