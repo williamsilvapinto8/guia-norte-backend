95314100e476c7ccef099704b1b30d9836c36a1a
William Silva Pinto <william.silvapinto@gmail.com>
2026-03-15 18:56:53 -0300

    Update settings.py
    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
