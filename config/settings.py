75f6b89274b6361654dfa730a76f0b040f3678ee
William Silva Pinto <william.silvapinto@gmail.com>
2026-03-18 02:32:23 -0300

    Update settings.py
    "https://app.guianorte.com.br",

--- config/settings.py
+++ config/settings.py
@@ -162,6 +162,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://guianorte.cocrias.com.br", # Domínio do seu frontend
     'CORS_ALLOWED_ORIGINS',
     'http://localhost:5173',
+    "https://app.guianorte.com.br",
+]
     # "http://127.0.0.1:8000", # Se você estiver testando localmente com outro porta
 ]
 
